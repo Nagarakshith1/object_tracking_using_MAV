8a48c454f6b054c340b8e4fcf50803d4fc20eb38
Nagarakshith1 <nagarakshithms@gmail.com>
2020-08-05 12:36:59 -0400

    first commit

---
+++ src/tracker/src/move_target.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+
+int main(int argc,char **argv){
+	ros::init(argc,argv,"move_target");
+	ros::NodeHandle n;
+	ros::Publisher pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1);
+	ros::Rate loop_rate(10);
+	
+	while(ros::ok()){
+		geometry_msgs::Twist twist_cmd;
+
+		twist_cmd.linear.x = 0;
+		twist_cmd.linear.y = 0;
+		twist_cmd.linear.z = 0;
+
+		twist_cmd.angular.x = 0;
+		twist_cmd.angular.y = 0;
+		twist_cmd.angular.z = 0;
+
+		pub.publish(twist_cmd);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	return 0;
+}
