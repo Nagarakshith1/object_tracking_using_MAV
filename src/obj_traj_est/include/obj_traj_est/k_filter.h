d77908333e5a79733dba41738c3df6749d694955
Ravi3191 <ravi.teja.konkimalla@gmail.com>
2020-08-06 02:01:31 -0400

    linked libraries in cmake

--- src/obj_traj_est/include/obj_traj_est/k_filter.h
+++ src/obj_traj_est/include/obj_traj_est/k_filter.h
@@ -20,13 +20,7 @@ public:
 
     KalmanFilter();
 
-    KalmanFilter(const state &x_init, const sq_matrix &q_init,const measurement_noise &r_init, const sq_matrix &p_init) : _x{x_init}, _Q{q_init}, _R{r_init}, _P{p_init} 
-    {
-        _H.setZero();
-        _H(0,0) = 1;
-        _H(1,1) = 1;
-        _H(2,2) = 1;
-    }
+    KalmanFilter(const state &x_init, const sq_matrix &q_init,const measurement_noise &r_init, const sq_matrix &p_init);
 
     void Initialize(const state &x_init,const sq_matrix &q_init, const measurement_noise &r_init,const sq_matrix &p_init);
 
--- src/obj_traj_est/src/k_filter.cpp
+++ src/obj_traj_est/src/k_filter.cpp
@@ -1,5 +1,19 @@
 #include <obj_traj_est/k_filter.h>
 
+KalmanFilter::KalmanFilter(){
+}
+
+KalmanFilter::KalmanFilter(const state &x_init, const sq_matrix &q_init,const measurement_noise &r_init, const sq_matrix &p_init){
+    _x = x_init;
+    _Q = q_init;
+    _R = r_init;
+    _P = p_init;
+    _H.setZero();
+    _H(0,0) = 1;
+    _H(1,1) = 1;
+    _H(2,2) = 1;
+}
+
 void KalmanFilter::Initialize(const state &x_init, const sq_matrix &q_init,const measurement_noise &r_init, const sq_matrix &p_init){
     _x = x_init;
     _Q = q_init;
